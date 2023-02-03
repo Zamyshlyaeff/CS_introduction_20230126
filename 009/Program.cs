9417f392e193728034574ca5f31bcbc2bdbe3d82
Nikolayz <zans55@yandex.ru>
2023-02-03 18:56:31 +0300

    Lesson 1 version 2

---
+++ 009/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести на экран четные числа от 1 до n
+//Console.WriteLine("Hello, World!");
+int i=2;
+int N=10;
+N=Convert.ToInt32(Console.ReadLine());
+// нц пока i<=10
+// кц
+while(i<=N)
+{
+    System.Console.WriteLine(i);
+    i=i+2;
+
+
+
+
+}
